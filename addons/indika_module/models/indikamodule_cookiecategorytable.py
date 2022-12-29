8f346de83f54c0a48ae54f79b30316501733eda7
indika <>
2022-12-29 20:05:18 +0000

    add Many2one filed to cookie category and cookie data tables

--- addons/indika_module/models/indikamodule_cookiecategorytable.py
+++ addons/indika_module/models/indikamodule_cookiecategorytable.py
@@ -5,5 +5,6 @@ from odoo import models, fields
 class INDIKAModuleCookiecategoryTable(models.Model):
 	_name = 'indikamodule.cookiecategorytable'
 	cookie_category_name = fields.Char('CookieCategoryName', required=True)
+	cookie_type_id = fields.Many2one('indikamodule.cookietypetable','CookieTypeId')
 
 
--- addons/indika_module/models/indikamodule_cookiedatatable.py
+++ addons/indika_module/models/indikamodule_cookiedatatable.py
@@ -5,5 +5,7 @@ from odoo import models, fields
 class INDIKAModuleCookiedataTable(models.Model):
 	_name = 'indikamodule.cookiedatatable'
 	cookie_name = fields.Char('CookieName', required=True)
+	cookie_category_id = fields.Many2one('indikamodule.cookiecategorytable','CookieCategoryId')
+
 
 
