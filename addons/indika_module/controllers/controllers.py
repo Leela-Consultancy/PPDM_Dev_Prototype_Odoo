ac9c9d95a719834b80e472616a8564971a7295bd
indika <>
2023-01-10 14:42:56 +0000

    add custom website view

--- addons/indika_module/controllers/controllers.py
+++ addons/indika_module/controllers/controllers.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
+class INDIKAModule(http.Controller):
 
-class PPDMWebsite(http.Controller):
-
-    @http.route('/indika/website/', auth='public')
+    @http.route('/indika/', auth='public')
     def index(self, **kw):
-        return http.request.render('indika_module.website', {
-            'teachers': ["Diana Padilla", "Hariharan", "Lester Vaughn"],
-        })
-
-#     @http.route('/academy/academy/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('academy.listing', {
-#             'root': '/academy/academy',
-#             'objects': http.request.env['academy.academy'].search([]),
-#         })
-
-#     @http.route('/academy/academy/objects/<model("academy.academy"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('academy.object', {
-#             'object': obj
-#         })
+        return "Hello, world"
