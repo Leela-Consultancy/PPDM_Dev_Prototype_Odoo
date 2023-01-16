c833f3776ee6f5bd8b5b285d459a6f9b0764c5fa
indika <>
2023-01-16 12:30:12 +0000

    Add Details page with dynamic data to layout

--- addons/indika_module/controllers/controllers.py
+++ addons/indika_module/controllers/controllers.py
@@ -1,16 +1,23 @@
-    # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from odoo import http
 
 class PPDMWebsite(http.Controller):
-
-    @http.route('/indika/website/', auth='public', website=True)
+    @http.route('/indika/website', auth='public', website=True)
     def index(self, **kw):
-
         webistes = http.request.env['indikamodule.websitestable'].search([])
         cookiedata = http.request.env['indikamodule.cookiedatatable'].search([])
-       # search webistes = http.request.env['indikamodule.websitestable'].search([])
+        # search webistes = http.request.env['indikamodule.websitestable'].search([])
         return http.request.render('indika_module.website', {
-               'webistes' : webistes,
-               'cookiedata' : cookiedata,
-           })
+            'webistes': webistes,
+            'cookiedata': cookiedata,
+        })
+
+    @http.route('/indika/website_details/<model("indikamodule.websitestable"):website>/', auth='public', website=True)
+    def website_details(self, website):
+        webistes = http.request.env['indikamodule.websitestable'].search([])
+        # search webistes = http.request.env['indikamodule.websitestable'].search([])
+        return http.request.render('indika_module.website_details', {
+            'webistes': website,
+        })
+
 
