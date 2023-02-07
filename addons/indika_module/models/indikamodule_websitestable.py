a5cd12f8b173ca56831ce6e8126a8d4d2788505e
indika <>
2023-02-07 09:09:10 +0000

    add relationship search

--- addons/indika_module/models/indikamodule_websitestable.py
+++ addons/indika_module/models/indikamodule_websitestable.py
@@ -6,4 +6,6 @@ class INDIKAModuleWebsitesTable(models.Model):
 	name = fields.Char('Title', required=True)
 	desc = fields.Char('Description', required=True)
 	url = fields.Char('Url', required=True)
+	cookie_type_id = fields.Many2one('indikamodule.cookietypetable', 'CookieTypeId')
+	vendor_id = fields.Many2one('indikamodule.vendortable', 'VendorId')
 
